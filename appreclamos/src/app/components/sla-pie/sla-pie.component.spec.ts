adcd517c2ee7e3b333eee26cf9d4c5175d3fc9bc
Melissa Silva <32807254+melipass@users.noreply.github.com>
2021-06-07 22:11:28 -0400

    SLA pie chart, fixes.

---
+++ appreclamos/src/app/components/sla-pie/sla-pie.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SlaPieComponent } from './sla-pie.component';
+
+describe('SlaPieComponent', () => {
+  let component: SlaPieComponent;
+  let fixture: ComponentFixture<SlaPieComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SlaPieComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SlaPieComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
