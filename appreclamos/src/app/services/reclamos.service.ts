ef1a816a82bf5e02ad0b470aabc6d03a7813035f
Mrnachoman11 <m.arenas.superstar@gmail.com>
2021-06-08 09:09:29 -0400

    Edit State Horrible
    Funca, pero es horrible

--- appreclamos/src/app/services/reclamos.service.ts
+++ appreclamos/src/app/services/reclamos.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Reclamos } from '../clases/reclamos';
-import {BehaviorSubject, Observable} from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 
 @Injectable({
@@ -12,32 +12,36 @@ export class ReclamosService {
   private reclamosAc: BehaviorSubject<Reclamos[]> = new BehaviorSubject<Reclamos[]>([]);
   constructor(private http: HttpClient) { }
 
-// tslint:disable-next-line:typedef
-getReclamos(): Observable<Reclamos[]>
-{
-  return this.reclamosAc.asObservable();
-}
-
-// tslint:disable-next-line:typedef
-setReclamos(reclamos: Reclamos[]): void
-{
-  this.reclamosAc.next(reclamos);
-}
+  // tslint:disable-next-line:typedef
+  getReclamos(): Observable<Reclamos[]> {
+    return this.reclamosAc.asObservable();
+  }
 
   // tslint:disable-next-line:typedef
-cargarReclamos(){
+  setReclamos(reclamos: Reclamos[]): void {
+    this.reclamosAc.next(reclamos);
+  }
+
+  // tslint:disable-next-line:typedef
+  cargarReclamos() {
     return this.http.get<Reclamos[]>('http://localhost:3000/reclamos');
   }
 
   // tslint:disable-next-line:typedef
-  cargarReclamosDeUnUsuario(rut: string)
-  {
+  cargarReclamosDeUnUsuario(rut: string) {
     return this.http.get<Reclamos[]>('http://localhost:3000/reclamos?rut=' + rut);
   }
 
   // tslint:disable-next-line:typedef
-  agregarReclamoDeUsuario(reclamo: Reclamos)
-  {
+  agregarReclamoDeUsuario(reclamo: Reclamos) {
     return this.http.post<Reclamos>('http://localhost:3000/reclamos', reclamo);
+  }
+
+  editarReclamo(reclamo: Reclamos) {
+    return this.http.put('http://localhost:3000/reclamos/' + reclamo.id, reclamo);
+  }
+
+  obtenerReclamo(id: Reclamos) {
+    return this.http.get('http://localhost:3000/reclamos' + id);
   }
 }
